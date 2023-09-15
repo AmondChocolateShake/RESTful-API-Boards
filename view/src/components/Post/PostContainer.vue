b9260b2306dd59af390961429135abc8c8d19d44
AmondChocolateShake <dongju230130@gmail.com>
2023-09-15 16:37:26 +0900

    fetch 후 게시글 리스트 가져오기 성공

--- view/src/components/Post/PostContainer.vue
+++ view/src/components/Post/PostContainer.vue
@@ -1,11 +1,41 @@
 <template lang="">
     <div id="postContainer">
-        
+        <!-- <PostElement/> -->
     </div>
 </template>
 <script>
+import {ref} from 'vue'
+// import PostElement from './PostElement.vue'
+
 export default {
-    name:"PostContainer"
+    name:"PostContainer",
+
+    props:{
+        setPostId:Function,
+        boardId:Number,
+    },
+
+    watch:{
+        boardId(id){
+            fetch(`http://localhost:3000/board/${id}/post`)
+        .then(res=>res.json())
+        .then(data=>{
+            this.posts.value=data.posts
+            console.log(this.posts.value)
+        })
+        }
+    },
+
+    data(){
+        const posts=ref([])
+
+        return{
+            posts
+        }
+    },
+    components:{
+        // PostElement
+    },
 
 }
 </script>
--- view/src/components/Post/PostElement.vue
+++ view/src/components/Post/PostElement.vue
@@ -1,11 +1,14 @@
 <template lang="">
     <div>
-        
+        <!-- {{post.title}} -->
     </div>
 </template>
 <script>
 export default {
+    name:"PostElement",
     
+
+
 }
 </script>
 <style lang="">
