d5bff60dd6c8ec759db8243a536ffcd7f4657d16
AmondChocolateShake <dongju230130@gmail.com>
2023-09-22 17:19:55 +0900

    get 메서드로 /board/:id 전송시 return id 하였음

---
+++ server/board_server/app/Http/Controllers/BoardController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Boards;
+
+
+class BoardController extends Controller
+{
+
+
+    public function get($id){
+        return $id;
+    }
+
+    public function create($name){
+
+    }
+
+    public function delete($id){
+
+    }
+
+    public function edit($id, $name){
+
+    }
+
+
+}
--- server/board_server/app/Models/Posts.php
+++ server/board_server/app/Models/Posts.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Posts extends Model
 {
     use HasFactory;
+    protected $table='posts';
+    protected $fillable=[
+        'title',
+        'created_at',
+        'author',
+        'context',
+        'board_id'];
 }
