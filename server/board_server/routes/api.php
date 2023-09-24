3d408cf2197a5f8d452f3a1ee9e1838a8f0e7756
AmondChocolateShake <dongju230130@gmail.com>
2023-09-24 22:37:11 +0900

    api 등록이 성공하였다. prefix를 이용해 board 그룹으로 api를 라우팅하였음. api/board/1 등의 방식으로 request

--- server/board_server/routes/api.php
+++ server/board_server/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\BoardController;
+use App\Http\Controllers\BoardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,8 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('board')->group(function(){
     Route::get('/{id}',[BoardController::class,'get']);
-    Route::get('',[BoardController::class,'getAll']);
-    Route::post('',[BoardController::class,'create']);
+    Route::get('/',[BoardController::class,'getAll']);
+    Route::post('/',[BoardController::class,'create']);
 });
 
 
