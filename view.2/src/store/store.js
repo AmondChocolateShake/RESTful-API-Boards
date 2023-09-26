20f5ec1c9743791ecff191140d5879dd81908423
AmondChocolateShake <dongju230130@gmail.com>
2023-09-26 10:47:23 +0900

    vuex설치 후 store 생성 하는 중

---
+++ view.2/src/store/store.js
@@ -0,0 +1,44 @@
+import { createStore } from "vuex";
+
+export default createStore({
+    state: {
+        title:'',
+        context:'',
+        author:'',
+        board_id:0,
+
+        
+    },
+    mutations: {
+        setTitle(title){
+            state.title=title;
+        },
+        setContext(context){
+            state.context=context;
+        },
+        setAuthor(author){
+            state.author=author;
+        },
+        setBoardId(id){
+            state.board_id=id;
+        },
+    },
+    actions: {
+        
+    },
+    getters: {
+        getFormData:state=>{
+            const formData={
+                title:state.title,
+                context:state.context,
+                board_id:state.board_id,
+            }
+        }
+    }
+})
+
+
+
+
+
+
