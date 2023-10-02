cac9c0471a6315dc47f604aeaa7945cf8964b67c
AmondChocolateShake <dongju230130@gmail.com>
2023-10-02 20:26:56 +0900

    postingView에서 form 데이터 전송 로직 추가중

--- view.2/src/components/PostingView.vue
+++ view.2/src/components/PostingView.vue
@@ -2,19 +2,38 @@
     <form action="">
         <div>
             제목:
-            <input type="text">
+            <input type= "text">
         </div>
         <div>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <textarea name= "" id= "" cols= "30" rows= "10"></textarea>
         </div>
         <div>
-            <button>등록</button>
+            <button @click= "submit">등록</button>
         </div>
     </form>
 </template>
 <script>
 export default {
-    
+    setup() {
+        const submit = () => {
+            fetch('http://localhost/api/board/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    title: '제목',
+                    author: '',
+                    content: '내용',
+                    boardId: 0
+                })
+            })
+        }
+
+        return{
+            submit
+        }
+    }
 }
 </script>
 <style lang="">
